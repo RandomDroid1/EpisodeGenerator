06cd7ca259e8d086d421e6c81d0b2ae7b6e32778
RandomDroid1 <lilspi321@gmail.com>
2025-09-11 22:00:34 -0700

    It is very tough to get the mean button to do what I want. I'm trying but it is just sending undefined. I'm close though, i can feel it in my BONES

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
-var factList = [
- "guh", "wuh"];
 
-var fact = document.getElementById("episode");
-var GenerateButton = document.getElementById("generator");
-var count = 0;
-if (GenerateButton) {
-  GenerateButton.addEventListener("click", displayFact);
+
+var text = document.getElementById("text");  // This establishes the variable for the text
+var generator = document.getElementById("generator"); // This establishes the variable for the Generator Button
+if (generator) {generator.addEventListener("click", generatenumbers);} // Makes sure the Generator is loaded, and runs the two functions
+if (generator) {generator.addEventListener("click", displayText);}
+
+
+function generatenumbers(){
+  episode = Math.floor((Math.random() * 7) + 1); // sets the value for the seasons, from 1-7 because thats how many seasons i need :3
 }
-function displayFact(){
-  fact.innerHTML = factList[count];
-  count++;
-  if (count == factList.length){
-    count = 0;
+var textList = episode; // 
+function displayText(){
+  text.innerHTML = textList; // sets the uh the uh text to be uh the uh episode and season. Man im such a good coder
   }
-}
