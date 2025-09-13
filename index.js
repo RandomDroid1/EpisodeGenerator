5e121014ca2ab6c21c1718e724ce3ffc129fea86
RandomDroid1 <lilspi321@gmail.com>
2025-09-12 18:00:21 -0700

    YAHAHAHAAHAHA THE BUTTON WORKS YOU STUPID PROGRAM AAAAAAAAAAHAHAHAHA (I successfully got a random number generator to put a random number on screen)

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
-
-
+episode = 0
 var text = document.getElementById("text");  // This establishes the variable for the text
 var generator = document.getElementById("generator"); // This establishes the variable for the Generator Button
-if (generator) {generator.addEventListener("click", generatenumbers);} // Makes sure the Generator is loaded, and runs the two functions
+if (generator) {generator.addEventListener("click", getRandomInt);} // Makes sure the Generator is loaded, and runs the two functions
 if (generator) {generator.addEventListener("click", displayText);}
 
 
 function generatenumbers(){
-  episode = Math.floor((Math.random() * 7) + 1); // sets the value for the seasons, from 1-7 because thats how many seasons i need :3
+   wagg = Math.floor((Math.random() * 7) + 1); // sets the value for the seasons, from 1-7 because thats how many seasons i need :3
+
 }
-var textList = episode; // 
+  function getRandomInt() {
+  mazda = Math.floor(Math.random() * 3);
+  console.log(mazda);
+}
+
+var textList = (episode); // 
 function displayText(){
-  text.innerHTML = textList; // sets the uh the uh text to be uh the uh episode and season. Man im such a good coder
+  text.innerHTML = mazda; // sets the uh the uh text to be uh the uh episode and season. Man im such a good coder
   }
+
+
+
