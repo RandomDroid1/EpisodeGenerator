de550217ab5fe5e93e2bae604fa8ff447275c2ab
RandomDroid1 <lilspi321@gmail.com>
2025-09-12 20:49:29 -0700

    it all works (episode + season rng)

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ var generator = document.getElementById("generator"); // This establishes the va
 if (generator) {generator.addEventListener("click", getRandomInt);} // Makes sure the Generator is loaded, and runs the two functions
 if (generator) {generator.addEventListener("click", displayText);}
 
-
-function generatenumbers(){
-   wagg = Math.floor((Math.random() * 7) + 1); // sets the value for the seasons, from 1-7 because thats how many seasons i need :3
-
-}
   function getRandomInt() {
-  mazda = Math.floor(Math.random() * 3);
-  console.log(mazda);
+  season = Math.floor(Math.random() * (8-1)+1); // selects the season number
+  if (season == 1) {
+    episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season one
+  }
+  if (season == 2) {
+    episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season two
+  }
+  if (season == 3) {
+    episode = Math.floor(Math.random() * (24-1)+1); // selects the episode number if season third
+  }
+  if (season == 4) {
+    episode = Math.floor(Math.random() * (24-1)+1); // selects the episode number if season four
+  }
+  if (season == 5) {
+    episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season five
+  }
+  if (season == 6) {
+    episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season six
+  }
+  if (season == 7) {
+    episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season seven
+  }
 }
 
 var textList = (episode); // 
 function displayText(){
-  text.innerHTML = mazda; // sets the uh the uh text to be uh the uh episode and season. Man im such a good coder
+  text.innerHTML = "Season " + season + ", Episode " + episode; // sets the uh the uh text to be uh the uh episode and season. Man im such a good coder <:
   }
 
 
