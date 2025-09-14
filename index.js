25f80b45c082b6c783b2779d8d048687ced04c9f
RandomDroid1 <lilspi321@gmail.com>
2025-09-14 15:58:49 -0700

    Starting to add buttons and such to the main page to link everything together. All the JS works perfectly

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ if (generator) {generator.addEventListener("click", displayText);}
         if (season == 7) {
           episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season seven
         }
+        
     }
     if (show == 2) { // The Newsroom
       show = "The Newsroom: "
@@ -43,21 +44,84 @@ if (generator) {generator.addEventListener("click", displayText);}
         if (season == 3) {
           episode = Math.floor(Math.random() * (7-1)+1); // selects the episode number if season two 
         }
+
     }
-    if (show == 3) {
+    if (show == 3) { // The Americans
       show = "The Americans: "
+      season = Math.floor(Math.random() * (7-1)+1); // selects the season number
+        if (season == 1) {
+          episode = Math.floor(Math.random() * (14-1)+1); // selects the episode number if season one
+        }
+        if (season == 2) {
+          episode = Math.floor(Math.random() * (14-1)+1); // selects the episode number if season two
+        }
+        if (season == 3) {
+          episode = Math.floor(Math.random() * (14-1)+1); // selects the episode number if season third
+        }
+        if (season == 4) {
+          episode = Math.floor(Math.random() * (14-1)+1); // selects the episode number if season four
+        }
+        if (season == 5) {
+          episode = Math.floor(Math.random() * (14-1)+1); // selects the episode number if season five
+        }
+        if (season == 6) {
+          episode = Math.floor(Math.random() * (11-1)+1); // selects the episode number if season six
+        }
     }
-    if (show == 4) {
+
+    if (show == 4) { // Severance
       show = "Severance: "
+      season = Math.floor(Math.random() * (3-1)+1); // selects the season number
+        if (season == 1) {
+          episode = Math.floor(Math.random() * (11-1)+1); // selects the episode number if season one 
+        }
+        if (season == 2) {
+          episode = Math.floor(Math.random() * (10-1)+1); // selects the episode number if season two 
+        }
     }
+
     if (show == 5) {
-      show = "Paradise: "
+      show = "Paradise: " // Paradise
+      season = Math.floor(Math.random() * (1-1)+1); // selects the season number // kept like this for when the next season releases, easy updating
+        if (season == 1) {
+          episode = Math.floor(Math.random() * (9-1)+1); // selects the episode number if season one
+        }
     }
-    if (show == 6) {
-      show = "Madam Secretary: "
+
+    if (show == 6) { // Madam Secretary
+      show = "Madam Secretary (if you can find somewhere to watch it): "
+      season = Math.floor(Math.random() * (7-1)+1); // selects the season number
+        if (season == 1) {
+          episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season one 
+        }
+        if (season == 2) {
+          episode = Math.floor(Math.random() * (24-1)+1); // selects the episode number if season two 
+        }
+        if (season == 3) {
+          episode = Math.floor(Math.random() * (24-1)+1); // selects the episode number if season third 
+        }
+        if (season == 4) {
+          episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season four 
+        }
+        if (season == 5) {
+          episode = Math.floor(Math.random() * (21-1)+1); // selects the episode number if season five 
+        }
+        if (season == 6) {
+          episode = Math.floor(Math.random() * (11-1)+1); // selects the episode number if season six
+        }
     }
-    if (show == 7) {
+    if (show == 7) { // Designated Survivor
       show = "Designated Survivor: "
+      season = Math.floor(Math.random() * (4-1)+1); // selects the season number
+        if (season == 1) {
+          episode = Math.floor(Math.random() * (22-1)+1); // selects the episode number if season one
+        }
+        if (season == 2) {
+          episode = Math.floor(Math.random() * (23-1)+1); // selects the episode number if season two
+        }
+        if (season == 3) {
+          episode = Math.floor(Math.random() * (11-1)+1); // selects the episode number if season third
+        }
     }
 }
 
